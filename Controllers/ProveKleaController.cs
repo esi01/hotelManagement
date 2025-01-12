99cdffbb87d43c5ea3fccfe8a7071d60690d0507
Klea Vataj <kleavataj56@gmail.com>
2025-01-12 18:42:16 +0100

    commit: prove

---
+++ Controllers/ProveKleaController.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Controllers
+{
+    public class ProveKleaController : Controller
+    {
+        private readonly HotelManagementDbContext _context;
+
+        public ProveKleaController(HotelManagementDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult ProveKlea()
+        {
+            return View();
+        }
+
+
+    }
+}
